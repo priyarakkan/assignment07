a734ff1678a86b7467fc4bf05216b51e84efa00e
priyarakkan <priyadharsinikdp@gmail.com>
2023-03-24 23:52:07 -0700

    updated
    updated

--- Assignment07/js/script2.js
+++ Assignment07/js/script2.js
@@ -5,94 +5,78 @@ let table = document.querySelector('#employees');
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
 let count = 0;
 
-
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
-    // PREVENT FORM SUBMISSION
+// PREVENT FORM SUBMISSION
     e.preventDefault();  
 
-    // GET THE VALUES FROM THE TEXT BOXES
-
-    let id = document.querySelector('#id').value;    
-    let name = String(document.querySelector('#name').value);    
-    let extension = document.querySelector('#extension').value;   
-    let email = document.querySelector('#email').value  
-    let inputState =String(document.querySelector('#department').value);
-       
-   
-    // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
-
-    let row = table.insertRow(0);
+// GET THE VALUES FROM THE TEXT BOXES
+        let id         = document.querySelector('#id').value;    
+        let name       = String(document.querySelector('#name').value);    
+        let extension  = document.querySelector('#extension').value;   
+        let email      = document.querySelector('#email').value  
+        let inputState =String(document.querySelector('#department').value);
+      
+// INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+        var newRow = table.insertRow(-1);
     
-    // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+// INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+        let cell1 = newRow.insertCell(0);
+        let cell2 = newRow.insertCell(1);
+        let cell3 = newRow.insertCell(2);
+        let cell4 = newRow.insertCell(3);
+        let cell5 = newRow.insertCell(4);
+        let text1 = document.createTextNode(id)
+        let text2 = document.createTextNode(name)
+        let text3 = document.createTextNode(extension)
+        let text4 = document.createTextNode(email)
+        let text5 = document.createTextNode(inputState)
+
+// APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
+        cell1.appendChild(text1)
+        cell2.appendChild(text2)
+        cell3.appendChild(text3)
+        cell4.appendChild(text4)
+        cell5.appendChild(text5)
+
+        newRow.appendChild(cell1)
+        newRow.appendChild(cell2)
+        newRow.appendChild(cell3)
+        newRow.appendChild(cell4)
+        newRow.appendChild(cell5)
     
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    let text1 = document.createTextNode(id)
-    let text2 = document.createTextNode(name)
-    let text3 = document.createTextNode(extension)
-    let text4 = document.createTextNode(email)
-    let text5 = document.createTextNode(inputState)
+        table.appendChild(newRow)   
 
-    // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
-    cell1.appendChild(text1)
-    cell2.appendChild(text2)
-    cell3.appendChild(text3)
-    cell4.appendChild(text4)
-    cell5.appendChild(text5)
-
-    row.appendChild(cell1)
-    row.appendChild(cell2)
-    row.appendChild(cell3)
-    row.appendChild(cell4)
-    row.appendChild(cell5)
-    
-    table.appendChild(row)
-
-    // CREATE THE DELETE BUTTON
+// CREATE THE DELETE BUTTON
     let deleteBtn = document.createElement('button')
     deleteBtn.className = 'btn btn-danger btn-sm text-right delete'
-
     let textDelete = document.createTextNode('X')
     deleteBtn.appendChild(textDelete)
-    
-    row.appendChild(deleteBtn)
-
+    newRow.appendChild(deleteBtn)
     document.querySelector('#employees').value = ''
+    table.appendChild(newRow)
+    
+// RESET THE FORM
+    form.reset()
+    
+// SET FOCUS BACK TO THE ID TEXT BOX
+    document.getElementById('id').focus();
 
-    table.appendChild(row)
-
-})
-
-
-
-    // RESET THE FORM
-  
-    // SET FOCUS BACK TO THE ID TEXT BOX
-
-    // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
-
-
+// INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
+    count++;
 
 // DELETE EMPLOYEE
+    newRow.addEventListener('click', (e) =>{
+        if (confirm('Are you sure to delete the employee?')) {
+            let row = e.target.parentElement;
+            newRow.deleteRow(row.rowIndex);
+            count--;
+            
+        }
+    });
 
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
-let outputElement = document.querySelector('#employeeCount')
-let employeeCount = 0
-outputElement.textContent = ` ( ${employeeCount} )`
-// DELETE EMPLOYEE
-employeeTable.addEventListener('click', (e) => {
-      if (confirm('Are you sure you want to delete this employee?')) {
-        const row = e.target.parentElement.parentElement;
-        employeeTable.deleteRow(row.rowIndex);
-        employeeCount--;
-        outputElement.textContent = employeeCount;
-      }
-  });
-  table.addEventListener('click', (e) =>{
-    
-    table.deleteRow();
-})
+
+}); 
+
+
